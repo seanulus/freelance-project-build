38a430e4796c0db1a700ad4c69e53464f95a9437
Sean McDermott <seanjmac@hotmail.com>
2018-06-08 15:26:58 -0700

    add jquery to toggle menu on mobile view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './scss/input.scss';
 import './css/output.css';
+
+
+$(document).ready(function() {
+  $(".burger").click(function(event) {
+    event.preventDefault();
+
+    $(".burger-nav").toggle();
+  });
+});
